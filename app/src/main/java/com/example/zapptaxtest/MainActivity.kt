e7d28b743b393ae28d4dc35b6e26bb3324ddc74d
Mariem <mariem.kallel@esprit.tn>
2023-10-02 23:45:50 +0100

    working on navigation

--- app/src/main/java/com/example/zapptaxtest/MainActivity.kt
+++ app/src/main/java/com/example/zapptaxtest/MainActivity.kt
@@ -2,10 +2,23 @@ package com.example.zapptaxtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.fragment.app.Fragment
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.example.zapptaxtest.home.presentation.HomeFragment
+import com.example.zapptaxtest.notifications.presentation.NotificationsFragment
+import com.example.zapptaxtest.settings.presentation.SettingsFragment
+import com.example.zapptaxtest.trips.presentation.TripListFragment
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView // Add this line
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
+
 }
--- app/src/main/java/com/example/zapptaxtest/notifications/presentation/NotificationsFragment.kt
+++ app/src/main/java/com/example/zapptaxtest/notifications/presentation/NotificationsFragment.kt
@@ -1,11 +1,12 @@
 package com.example.zapptaxtest.notifications.presentation
 
 import android.os.Bundle
+import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import androidx.fragment.app.Fragment
 import com.example.zapptaxtest.R
+
 
 class NotificationsFragment : Fragment() {
 
@@ -19,7 +20,9 @@ class NotificationsFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_trip_list, container, false)
+        return inflater.inflate(R.layout.fragment_notifications, container, false)
     }
+
+
 
 }
