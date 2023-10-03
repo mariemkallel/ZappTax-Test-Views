8958e6543e941891aba9421a7e35cc812f4884fe
Mariem <mariem.kallel@esprit.tn>
2023-10-03 11:16:15 +0100

    added bottom navigation bar

--- app/src/main/java/com/example/zapptaxtest/MainActivity.kt
+++ app/src/main/java/com/example/zapptaxtest/MainActivity.kt
@@ -2,23 +2,33 @@ package com.example.zapptaxtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import androidx.navigation.NavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
-import com.example.zapptaxtest.home.presentation.HomeFragment
-import com.example.zapptaxtest.notifications.presentation.NotificationsFragment
-import com.example.zapptaxtest.settings.presentation.SettingsFragment
-import com.example.zapptaxtest.trips.presentation.TripListFragment
-import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.example.zapptaxtest.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
     private lateinit var navController: NavController
-    private lateinit var bottomNavigationView: BottomNavigationView // Add this line
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        setupNavigation()
+    }
+
+    private fun setupNavigation() {
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_main_fragment) as NavHostFragment
+        navController = navHostFragment.navController
+        binding.bottomNavigationBar.setupWithNavController(navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return navController.navigateUp() || super.onSupportNavigateUp()
     }
 
 }
